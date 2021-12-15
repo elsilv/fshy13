fb030809ebc20742cc7c8d19ca3155a546f32b78
elsilv <elsi.silvennoinen@gmail.com>
2021-12-15 13:19:21 +0200

    13.4

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const express = require('express')
 require('express-async-errors')
 const cors = require('cors')
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
-const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+/*
+const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const testRouter = require('./controllers/tests')
 const mongoose = require('mongoose')
+*/
 
 const app = express()
 
@@ -24,38 +26,76 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+//app.use(express.static('build'))
 app.use(middleware.tokenExtractor)
 
+/*
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/testing', testRouter)
+*/
 
 app.use(middleware.errorHandler)
 
-// heroku health check
-app.get('/health', (req, res) => {
-  res.send('ok')
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
 })
 
-app.get('/version', (req, res) => {
-  res.send('2')
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
 })
 
-main() 
+app.delete('/api/blogs/:id', async (req, res) => {
+  console.log(req.params.id)
+  var id = req.params.id
+  const blog = await Blog.findByPk(id)
+  console.log(blog)
+  
+  if(blog === null ) {
+    console.log('Blog not found')
+  } else {
+    await blog.destroy()
+  }
+})
 
 module.exports = app
