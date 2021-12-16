02cd942ed071140f7ac96e593e924fb086c2d649
elsilv <elsi.silvennoinen@gmail.com>
2021-12-16 13:30:44 +0200

    13.9

--- utils/middleware.js
+++ utils/middleware.js
@@ -9,6 +9,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'ReferenceError') {
+    return response.status(401).json({ error: 'username should be email' })
   }
 
   next(error)
